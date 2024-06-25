1470236a9be1e40db22919570f34b6b3072e6f68
Gaspar_Bucuane <gasparbucuane@gmail.com>
2024-06-25 14:50:07 +0200

    Initial commit

---
+++ item/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'item'
+
+urlpatterns = [
+    path('', views.items, name='items'),
+    path('new/', views.new, name='new'),
+    path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/editt/', views.edit, name='edit'),
+    
+]
